6a20feca1e68ebf2d4eab4c83d1b80b80e0ab23f
Chris Clement <chrisclement@Chriss-Air.connect>
2021-12-07 13:04:57 +0000

    Project set up

---
+++ app.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+# Sinatra class
+class DiaryManager < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  run! if app_file == $0
+end
